89f05c70d09580ac2e2dc7fbfaad8a04716bc0da
juninho chiodi <juninho.junirj@gmail.com>
2023-05-25 15:53:58 -0300

    commit

--- App/rotas.php
+++ App/rotas.php
@@ -8,6 +8,18 @@ $parse_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch($parse_uri) {
 
+    // Fazendo a função de exportar Banco de Dados / Fazer dps a função de restaurar Banco de Dados
+    case '/exportar':
+        $return_var = NULL;
+        $output = NULL;
+        $command = 'C:/"Program Files"/MySQL/"MySQL Server 8.0"/bin/mysqldump -u root -p etecjau -P 3307 -h localhost db_bancodigital > C:/Dev/file.sql';
+        exec($command, $output, $exit_code);
+
+        var_dump($exit_code);
+
+        echo "deu certo.";
+    break;
+
     case "/":
         include 'View/modules/Pg-Inicial/Pg-Inicial.php';
     break;
